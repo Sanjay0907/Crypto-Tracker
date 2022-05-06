05d3bbbf9224e36f4f5460c79c19478c204b1b21
Sanjay0907 <sanjaykumardas7399008072@gmail.com>
2022-05-07 00:03:22 +0530

    updates

--- lib/models/API.dart
+++ lib/models/API.dart
@@ -5,7 +5,7 @@ class API {
   static Future<List<dynamic>> getMarkets() async {
     try {
       Uri requestPath = Uri.parse(
-          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=20&page=1");
+          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1");
 
       var response = await http.get(requestPath);
       var decodedResponse = jsonDecode(response.body);
